9eb0e09c9bd5ada08e471478e7ccead18b04e91c
SUMAN K <suman@mosaique.link>
2023-11-24 16:47:06 +0530

    "first commmit"

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -6,26 +6,37 @@
 from django.urls import path, re_path
 from apps.home import views
 
+# from LED import Lviews
+
 urlpatterns = [
 
     # The home page
     path('', views.index, name='home'),
-    path('user_list', views.user_list, name='user_list'),
-    path('add_user', views.add_user, name="add_user"),
-    path('delete_user', views.delete_user, name='delete_user'),
-    path('update_user/<str:pk>',views.update_user,name='update_user'),
+    # path('user_list', views.user_list, name='user_list'),
+    # path('add_user', views.add_user, name="add_user"),
+    # path('delete_user', views.delete_user, name='delete_user'),
+    # path('update_user',views.update_user,name='update_user'), #/<str:pk>
     path('house_list',views.house_list, name="house_list"),
     path('add_house',views.add_house,name="add_house"),
     path('farm_manage',views.farm_manage, name="farm_manage"),
-    #  path('reset_password',views.reset_password, name="reset_password"),
     path('house_lights',views.house_lights, name='house_lights'),
     path('LED_control',views.LED_control, name='LED_control'),
+    path('LED_control/<int:farm_id>/', views.LED_control, name='LED_control_farm_id'),
     path('plant_setting',views.plant_setting, name='plant_setting'),
     path('add_plant',views.add_plant, name='add_plant'),
     path('update_plant/<str:pk>',views.update_plant,name='update_plant'),
     path('delete_plant/<int:plant_id>/', views.delete_plant, name='delete_plant'),
+  
    
     # Matches any html file
     # re_path(r'^.*\.*', views.pages, name='pages'),
+    
+    
+    # MQTT
+     path('suman', views.suman, name='suman'),
+    path('toggle_led/', views.toggle_led, name='toggle_led'),
+    path('update_led_status/',views. update_led_status, name='update_led_status'),
+    path('publish_data/', views.publish_data, name='publish_data'),
+     path('mqtt_sub/', views.mqtt_sub, name='mqtt_sub'),
 
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -5,19 +5,17 @@
 
 from django import template
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseServerError, JsonResponse
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseServerError
 from django.template import loader
 from django.urls import reverse
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import UserAddForm,PlantForm
-from .models import User,Plant
+from .forms import PlantForm
+from .models import Plant, Farm, House, Line, Pole, LED
 from django.core.paginator import Paginator
-import json
 from django.contrib import messages
-from django.core.mail import send_mail
-from django.conf import settings
-import uuid
-from django.utils.html import format_html
+from apps.authentication.models import Profile
+from django.contrib.auth.models import User
+
 
 @login_required(login_url="/login/")
 def index(request):
@@ -30,11 +28,27 @@ def house_lights(request):
     html_template = loader.get_template('home/house-lights.html')
     return HttpResponse(html_template.render(context, request))
 
-def LED_control(request):
-    context = {'segment' : 'LED_control'}
-    html_template = loader.get_template('home/switch.html')
-    return HttpResponse(html_template.render(context, request))
+def LED_control(request,farm_id=None):
+    choice_farm = Farm.objects.all()
+    selected_farm_name = None
+    if request.method == 'POST':
+        farm_id = request.POST.get('selected_farm')
+    else:
+        farm_id = choice_farm.first().farm_id
+    
+    if farm_id:
+            houses = House.objects.filter(farm_id=farm_id)
+            selected_farm = Farm.objects.get(pk=farm_id)
+            selected_farm_name = selected_farm.farm_name   
+    
+    context = {'segment': 'LED_control', 'choice_farm': choice_farm, 'houses': houses,'selected_farm_name': selected_farm_name, 'farm_id': farm_id}
 
+    return render(request, 'home/LED-control.html', context)
+
+
+
+
+    #List of plant (settings.html)
 def plant_setting(request):
     try:
         plant_list = Plant.objects.all().order_by('plant_id')
@@ -42,7 +56,6 @@ def plant_setting(request):
         page_list = request.GET.get('page')
         page = page.get_page(page_list)
         context = {'segment':'plant_settings', 'plant_list': plant_list,'page': page}
-        print('context>>>>',context)
         if request.method == "GET":
             html_template = loader.get_template('home/settings.html')
             return HttpResponse(html_template.render(context, request))
@@ -69,130 +82,6 @@ def update_plant(request,pk):
     except BrokenPipeError as e:
         print('exception BrokenPipeError', e)
         return HttpResponseServerError()
-
-# Add new user
-
-@login_required
-def add_user(request):
-    if request.method == 'GET':
-        context = {}
-        return render(request, 'home/add-user.html', context)
-    else:
-    #     # form = UserAddForm(request.POST)
-    
-        username = request.POST['username']
-        email = request.POST['email']
-        print('email>>',email)
-        print('username>>',username)
-    #     user_id = request.POST['mapped_under']
-    #     print('request>>>>>',request.POST['email'])
-    #     email = request.POST['email']
-    #     user_id = request.user.id
-    #     print('user_id',user_id)
-    #     if form.is_valid():
-    #         if User.objects.filter(email=email).exists():
-    #             error_message = "このメールはすでに存在します。別のメールをお試しください。"
-    #             return render(request, 'home/add-user.html', {'form': form, 'error_message': error_message}) # Redirect to the same page
-    #         else:
-    #                 form.save()
-    #                 farm_id = str(uuid.uuid4())[:6].upper()
-    #                 reset_token = str(uuid.uuid4())
-    #                 link = f'https://uvb-beamtec.mosaique.link/reset_password{reset_token}'
-    #                 subject = 'パスワードの設定'
-    #                 message = f'''
-    #                 光防除システム管理サイトログイン
-
-    #                 光防除システム管理サイトへようこそ！
-    #                 下記の「パスワードの設定」をクリックして進んでください。
-
-    #                 パスワードの設定：{link}
-
-    #                 ★！現段階ではまた登録は完了しておりません！★
-    #                 ※ご本人様確認のため、上記URLへ「24時間以内」にアクセスしアカウントの本登録を完了いただけますようお願いいたします。
-
-    #                 農場ID：{farm_id}
-    #                 ID：{email}
-
-    #                 ご不明な点がございましたら、このメールへご返信いただくか、
-    #                 info@beam~ までご連絡ください。'''
-                    
-    #                 from_email = settings.EMAIL_HOST_USER
-    #                 recipient_list = [email]
-
-    #                 send_mail(subject, message, from_email, recipient_list)
-
-    #                 success_message = "ユーザが正常に追加されました"
-    #                 messages.success(request, success_message)
-    #                 return redirect('/user_list')
-        # else:
-        #     # Handle the case when the form is not valid
-        #     error_message = "無効なフォームデータです。フォームフィールドを確認して、もう一度やり直してください。"
-        #     return render(request, 'home/add-user.html', {'form': form, 'error_message': error_message}) # Redirect to the same page
-
-
-# Get user list and Active/Disabled user
-
-def user_list(request):
-    try:
-        user_list = User.objects.filter(isDeleted=False).order_by('user_id')
-        page = Paginator(user_list, 5)
-        page_list = request.GET.get('page')
-        page = page.get_page(page_list)
-        if request.method == "GET":
-            return render(request, 'home/admin.html', {'page': page})
-        elif request.method == "POST":
-            user_id = request.POST['user_id']
-            is_active = request.POST['is_active']
-            user = get_object_or_404(User, pk=user_id)
-            user.isActive = is_active
-            user.save()
-            success_message = "ユーザーステータスの変更に成功しました"  # success message here
-            return render(request, 'home/admin.html', {'page': page, 'success_message': success_message})
-    except BrokenPipeError as e:
-        print('exception BrokenPipeError', e)
-        return HttpResponseServerError()
-
- # Update User
-
-def update_user(request, pk):
-    show = 'true'
-    user = User.objects.get(user_id=pk)
-    if request.method == 'POST':
-        form = UserAddForm(request.POST, instance=user)
-        if form.is_valid():
-            email = form.cleaned_data.get('email')
-            if User.objects.filter(email=email).exclude(user_id=pk).exists():
-                error_message = "このメールはすでに存在します。別のメールをお試しください。"
-                return render(request, 'home/add-user.html', {'form': form, 'error_message': error_message})
-            form.save()
-            update_success_message = 'ユーザー詳細が正常に更新されました'
-            messages.success(request, update_success_message)
-            return redirect('/user_list')
-
-    else:
-        form = UserAddForm(instance=user)
-    context = {"form": form, "show": show}
-    return render(request, 'home/add-user.html', context)
-
-
-# Delete user api
-def delete_user(request):
-    if request.method == 'POST':
-        data = json.loads(request.body)
-        # Extract the user_id from the request data
-        user_id = data.get('user_id')
-        if user_id:
-            # Assuming your model is named 'User'
-            user = get_object_or_404(User, pk=user_id)
-            # user.isDeleted = True
-            user.delete()
-            response_data = {'message': 'ユーザー削除成功されました。'}
-            return JsonResponse(response_data, status=200)
-        else:
-            response_data = {'error': 'ユーザーIDが提供されていないです。'}
-            return JsonResponse(response_data, status=400)
-    else:
-        return JsonResponse({'error': '無効なリクエストメソッド'}, status=400)
 
 
 @login_required(login_url="/login/")
@@ -227,9 +116,69 @@ def house_list(request):
     return HttpResponse(html_template.render(context, request))
 
 def add_house(request):
-    context={}
-    html_template = loader.get_template('home/add-house.html')
-    return HttpResponse(html_template.render(context, request))
+    choice_plant = Plant.objects.all()
+    choice_farm = Farm.objects.all()
+    if request.user.is_authenticated:
+        current_user_id = request.user.id
+        mapped_profiles = Profile.objects.filter(mapped_under=current_user_id)
+        
+    choice_user = [profile.user for profile in mapped_profiles]
+    # print("----------->",choice_user.user.id)
+    context = {'segment':'add_house', 'choice_plant': choice_plant, 'choice_farm': choice_farm, 'choice_user': choice_user}
+    if request.method == "GET":
+        html_template = loader.get_template('home/add-house.html')
+        return HttpResponse(html_template.render(context, request))
+    else:
+        user_id = request.POST.get("farmerName")
+        house_name = request.POST.get("houseNameReg")
+        plant_id = request.POST.get("plantNameReg")
+        farm_id = request.POST.get("farmNameReg")
+        memo = request.POST.get("memoReg")
+        # lane_count = request.POST.get("laneCount")
+        # pole_count_per_lane = request.POST.get("laneCountPerPole")
+        # led_count_per_pole = request.POST.get("ledCountPerpole")
+        lane_count = int(request.POST.get("laneCount"))
+        pole_counts = [int(count) for count in request.POST.get("laneCountPerPole").split(",")]
+        led_counts = [int(count) for count in request.POST.get("ledCountPerpole").split(",")]
+
+        print("------------",user_id,house_name,plant_id,farm_id,memo,lane_count, pole_counts,led_counts)
+        house = House(
+            house_name=house_name,
+            memo=memo
+        )
+        if user_id:
+            user = User.objects.get(pk=user_id)
+            house.user = user
+        if plant_id:
+            house.plant_id = plant_id
+        if farm_id:
+            farm_id
+            house.farm_id = farm_id
+        house.save()
+
+        for i in range(1, lane_count + 1):
+            line = Line(
+                house=house,
+                pole_count=pole_counts[i - 1]
+            )
+            line.save()
+
+            # Create Pole and LED instances
+            for j in range(1, pole_counts[i - 1] + 1):
+                pole = Pole(
+                    line=line,
+                    led_count=led_counts.pop(0)
+                )
+                pole.save()
+
+                # Create LED instances
+                for k in range(1, pole.led_count + 1):
+                    led = LED(
+                        pole=pole
+                    )
+                    led.save()
+
+        return redirect('/house_list')
 
 def  farm_manage (request):
     context={}
@@ -272,3 +221,254 @@ def delete_plant(request, plant_id):
         print('exception BrokenPipeError', e)
         return HttpResponseServerError()
     
+    
+# -------------------------------------MQTT  for Raspberry pi ----------------------------------
+# Create your views here.
+from django.shortcuts import render
+from django.utils import timezone
+import paho.mqtt.client as mqtt
+import json
+from datetime import date, datetime, timedelta
+from .models import LineSensor
+from django.http import JsonResponse , HttpResponse
+
+
+# Global variable to store the latest sensor data
+latest_sensor_data = {
+    'temperature': 0.0,
+    'humidity': 0.0,
+    'soil_moisture': 0.0,
+}
+
+# Variable to store the timestamp of the last received sensor data
+last_sensor_data_timestamp = None
+
+# Timeout duration in seconds (adjust as needed)
+timeout_duration = 60  # Assuming a timeout of 60 seconds
+raspberry_pi_id =None
+
+# Function to handle MQTT messages
+# Function to handle MQTT messages
+def on_message(client, userdata, message):
+    global latest_sensor_data, last_sensor_data_timestamp, raspberry_pi_id
+    try:
+        payload = json.loads(message.payload.decode())
+        temperature = round(payload.get("temperature"), 3)
+        humidity = round(payload.get("humidity"), 3)
+        soil_moisture = round(payload.get("soil_moisture"), 3)
+        raspberry_pi_id = "Rpi1"
+
+        # Reset the timestamp of the last received sensor data to indicate new data received
+        last_sensor_data_timestamp = datetime.now()
+
+        # Check if a record for the current day already exists
+        today = date.today()
+        existing_record = LineSensor.objects.filter(date=today).first()
+
+        if existing_record:
+            # Update the existing record for the current day
+            existing_record.temperature = temperature
+            existing_record.humidity = humidity
+            existing_record.soil_moisture = soil_moisture
+            existing_record.save()
+        else:
+            # Create a new record for the current day
+            LineSensor.objects.create(
+                date=today,
+                time=timezone.now().time(),
+                raspberry_pi_id=raspberry_pi_id,
+                temperature=temperature,
+                humidity=humidity,
+                soil_moisture=soil_moisture,
+            )
+
+    except json.JSONDecodeError as e:
+        print("Error decoding JSON:", e)
+        print("Problematic payload:", message.payload.decode())
+        return
+
+    # Update the global variable with the latest sensor data
+    latest_sensor_data = {
+        'temperature': temperature,
+        'humidity': humidity,
+        'soil_moisture': soil_moisture,
+    }
+
+
+# MQTT broker configuration
+broker_address = "localhost"  # replace with your MQTT broker address
+port = 1883
+topic = "rpi_to_ec2_topic"
+
+# broker_address = "52.193.119.75"
+# port = 1883
+# username = "dht"
+# password = "dht123"
+
+
+# Set up MQTT client and subscribe to the topic
+mqtt_client = mqtt.Client()
+# mqtt_client.username_pw_set(username, password)
+mqtt_client.on_message = on_message
+mqtt_client.connect(broker_address, port, 60)
+mqtt_client.subscribe(topic)
+mqtt_client.loop_start()  # Start the MQTT client loop in a non-blocking way
+
+# Django view function
+def suman(request):
+    print("varuthuuuuu")
+    global latest_sensor_data, last_sensor_data_timestamp
+
+    # Check if there is a timeout
+    if last_sensor_data_timestamp is not None:
+        time_difference = datetime.now() - last_sensor_data_timestamp
+        if time_difference.total_seconds() > timeout_duration:
+            # If there is a timeout, set all sensor values to zero
+            latest_sensor_data = {
+                'temperature': 0.0,
+                'humidity': 0.0,
+                'soil_moisture': 0.0,
+            }
+
+    # If latest_sensor_data is not None, use the received data; otherwise, set default values
+    temperature = latest_sensor_data['temperature']
+    humidity = latest_sensor_data['humidity']
+    soil_moisture = latest_sensor_data['soil_moisture']
+
+    context = {
+        'temperature': temperature,
+        'humidity': humidity,
+        'soil_moisture': soil_moisture,
+        "Rbi": raspberry_pi_id
+    }
+    print(context)
+
+    return render(request, 'home/suman.html', context)
+
+def publish_data(request):
+    global mqtt_client
+
+    # You can add more validation or security checks here if needed
+
+    # Request data from the MQTT publisher
+    request_data = {"request": "publish_data"}
+    request_json = json.dumps(request_data)
+    mqtt_client.publish("request_topic", payload=request_json, qos=1)
+
+    # You can add a response message or handle the response as needed
+    response_message = "Data publish request sent to the publisher."
+
+    return JsonResponse({"message": response_message})
+
+def toggle_led(request):
+    if request.method == 'POST' and 'led_number' in request.POST:
+        led_number = request.POST['led_number']
+        print('LED number received:', led_number)
+        # Add your logic to handle the LED number as needed
+
+        # You can return a JsonResponse if needed
+        return JsonResponse({'message': 'LED number received successfully'})
+
+    return JsonResponse({'error': 'Invalid request'}, status=400)
+
+
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def update_led_status(request):
+    if request.method == 'POST':
+        led_no = request.POST.get('ledNo')
+        is_checked = request.POST.get('isChecked')
+        print ("data", led_no, is_checked)
+        
+        # Perform actions based on LED status (on/off)
+        # Your logic to update LED status goes here
+
+        response_data = {'message': 'LED status updated successfully'}
+        return JsonResponse(response_data)
+    else:
+        response_data = {'message': 'Invalid request method'}
+        return JsonResponse(response_data, status=400)
+   
+
+import json
+import threading
+import paho.mqtt.client as mqtt
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.dispatch import Signal, receiver
+
+# Define signals for manual input and sending relay information
+manual_input_signal = Signal()
+send_relay_information_signal = Signal()
+
+
+broker_address = "localhost"
+port = 1883
+
+# broker_address = "52.193.119.75"
+# port = 1883
+# username = "dht"
+# password = "dht123"
+
+topic_rpi_to_ec2 = "rpi_to_ec2_topic"
+topic_ec2_to_rpi = "ec2_to_rpi_topic"
+
+ec2_client = mqtt.Client()
+
+# ec2_client.username_pw_set(username, password)
+relay_number_input = None
+input_lock = threading.Lock()
+relay_status = None
+relay_status_lock = threading.Lock()
+
+def on_message_ec2(client, userdata, message):
+    global relay_status
+    payload = json.loads(message.payload.decode())
+    print(f"Received message from Raspberry Pi: {payload}")
+
+    with relay_status_lock:
+        relay_status = payload.get("relay_status")
+
+ec2_client.connect(broker_address, port, 60)
+ec2_client.on_message = on_message_ec2
+ec2_client.subscribe(topic_rpi_to_ec2)
+
+@receiver(manual_input_signal)
+def manual_input(sender, led_no, **kwargs):
+    global relay_number_input
+    try:
+        relay_number = int(led_no)
+        with input_lock:
+            if relay_number != relay_number_input:
+                relay_number_input = relay_number
+                with relay_status_lock:
+                    relay_status = {"relay_number": relay_number}
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+
+@receiver(send_relay_information_signal)
+def send_relay_information(sender, **kwargs):
+    global relay_status
+    with relay_status_lock:
+        if relay_status is not None:
+            # ec2_client.publish(topic_ec2_to_rpi, json.dumps({"relay_control": relay_status}))
+            relay_status = None
+
+def mqtt_sub(request):
+    led_no = None
+    if request.method == 'POST':
+        led_no = request.POST.get('ledNo')
+        is_checked = request.POST.get('isChecked')
+        print("data >>>>>>>>>>>>>", led_no, is_checked)
+        ec2_client.publish(topic_ec2_to_rpi, json.dumps({"relay_control": led_no}))
+        # Trigger the manual input signal
+        manual_input_signal.send(sender=None, led_no=led_no)
+
+        # Trigger the send relay information signal
+        send_relay_information_signal.send(sender=None)
+
+    context = {}  # Use an empty dictionary as the context
+    return render(request, 'home/suman.html', context)
+
